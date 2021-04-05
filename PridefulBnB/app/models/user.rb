87b4eb608de4c388fea888d3309ffe37258d6b35
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-05 16:04:22 -0700

    Finish auth user model

---
+++ PridefulBnB/app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, :first_name, :last_name, 
+    :birth_date, :current_location, :phone_number, :gender, :identity, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: {min: 6, allow_nil: true}
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+  
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    if user && is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.session_token.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
