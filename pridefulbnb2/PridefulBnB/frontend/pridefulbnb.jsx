6f2e857be8277b5b375fb3bdde8f76bf60e9b8e1
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-05 23:22:11 -0700

    Run npm installs to set up project with react and redux

---
+++ pridefulbnb2/PridefulBnB/frontend/pridefulbnb.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+// import Root from './components/root';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  ReactDOM.render(<h1>React is Working</h1>, root)
+})
