5f31ebe9653977ccc43c34570f28ee25387e467f
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-05 22:56:33 -0700

    Copy PridefulBnB into a new folder to fix the enoent error

---
+++ pridefulbnb2/PridefulBnB/db/migrate/20210406013257_remove_phone_number_from_users.rb
@@ -0,0 +1,6 @@
+class RemovePhoneNumberFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :phone_number, :integer
+    add_column :users, :phone_number, :string
+  end
+end
