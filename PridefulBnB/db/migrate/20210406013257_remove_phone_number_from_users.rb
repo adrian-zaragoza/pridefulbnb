7048e005a9ddfe2450da112ff22fb0c6c811d095
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-05 22:58:27 -0700

    Delete original project folder to keep the copy project going forward

--- PridefulBnB/db/migrate/20210406013257_remove_phone_number_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemovePhoneNumberFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :phone_number, :integer
-    add_column :users, :phone_number, :string
-  end
-end
--- PridefulBnB/db/migrate/20210406014622_add_constraint_to_column_from_users.rb
+++
@@ -1,5 +0,0 @@
-class AddConstraintToColumnFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :users, :phone_number, false
-  end
-end
