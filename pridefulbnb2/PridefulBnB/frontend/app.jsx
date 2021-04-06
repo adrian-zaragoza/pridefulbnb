66e68c0ad95bcb537145f8dda76cf1497673f7b5
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-06 14:21:51 -0700

    Complete signup form

---
+++ pridefulbnb2/PridefulBnB/frontend/app.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+import SignupContainer from './components/session/signup_container';
--- pridefulbnb2/PridefulBnB/frontend/components/session/signup.jsx
+++ pridefulbnb2/PridefulBnB/frontend/components/session/signup.jsx
@@ -14,6 +14,8 @@ class Signup extends React.Component{
       gender: '',
       identity: ''
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   update(field) {
@@ -33,6 +35,41 @@ class Signup extends React.Component{
       <div className="signup-form">
         <h2>Enjoy the full experience of LGBTQ hospitality</h2>
         <p>1 million LGBTQ-friendly accommodations in 200 countries.</p>
+
+        <form>
+          <label>First name
+            <input type="text" value={this.state.firstName} onChanged={this.update('firstName')}/>
+          </label>
+          <label>Last name
+            <input type="text" value={this.state.lastName} onChanged={this.update('lastName')} />
+          </label>
+          <label>Your city
+            <input type="text" value={this.state.currentLocation} onChanged={this.update('currentLocation')} />
+          </label>
+          <label>Your e-mail
+            <input type="text" value={this.state.email} onChanged={this.update('email')} />
+          </label>
+          <label>Password
+            <input type="password" value={this.state.password} onChanged={this.update('password')} />
+          </label>
+          <label>Date of birth
+            <input type="date" value={this.state.birthDate} onChanged={this.update('birthDate')} />
+          </label>
+          <label>Phone number
+            <input type="text" value={this.state.phoneNumber} onChanged={this.update('phoneNumber')} />
+          </label>
+          <label>Please select your gender
+            <input type="radio" value={this.state.gender} onChanged={this.update('gender')} />Male
+            <input type="radio" value={this.state.gender} onChanged={this.update('gender')} />Female
+            <input type="radio" value={this.state.gender} onChanged={this.update('gender')} />Transgender
+            <input type="radio" value={this.state.gender} onChanged={this.update('gender')} />Other
+          </label>
+          <label>What do you identify as?
+            <input type="radio" value={this.state.identity} onChanged={this.update('identity')} />LGBTQ+
+            <input type="radio" value={this.state.identity} onChanged={this.update('identity')} />LGBTQ+ Ally
+          </label>
+          <button onClick={this.handleSubmit}>Sign Up</button>
+        </form>
       </div>
     );
   }
