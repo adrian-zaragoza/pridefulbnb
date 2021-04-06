64a8767f0f5f8516abd32e719050d59614d9d0fc
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-05 22:14:07 -0700

    Finish testing backend for session and user auth

--- PridefulBnB/app/controllers/api/sessions_controller.rb
+++ PridefulBnB/app/controllers/api/sessions_controller.rb
@@ -10,5 +10,6 @@ class Api::SessionsController < ApplicationController
 
   def destroy
     logout!
+    render json: "user has logged out"
   end
 end
--- PridefulBnB/app/models/user.rb
+++ PridefulBnB/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
 
-    if user && is_password?(password)
+    if user && user.is_password?(password)
       return user
     else
       return nil
