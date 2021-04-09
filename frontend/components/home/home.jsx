7032b6e4eeff51d4bbb41774b7f3d4ec23d79ecb
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-09 16:52:34 -0700

    Complete the icon bar with text

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
-//import shield from "../../../app/assets/images/shield.png";
 
 export default () => (
   <div className="home">
     <div className="icon-bar">
 
       <div className="icon">
-        <img src="../../../app/assets/images/shield.png" alt="shield icon" />
+        <img src={window.shieldIcon} alt="shield icon" />
         <p>New free cancellation policy.</p>
-        <br/>
         <p>Covid 19 Safety Protocol</p>
       </div>
       <div className="icon">
-        <img src={window.shieldIcon} alt="people connection icon" />
+        <img src={window.connectionIcon} alt="people connection icon" />
         <p>{"Connect with other pridefulb&b"}</p>
-        <br />
         <p>travelers during your trip</p>
       </div>
       <div className="icon">
-        {/* <img src={shield} alt="rainbow icon" /> */}
+        <img src={window.rainbowIcon} alt="rainbow icon" />
         <p>Help LGBTQ nonprofits</p>
-        <br />
         <p>when you book</p>
       </div>
       <div className="icon">
-        <img src="../../../app/assets/images/invoice.svg" alt="invoice icon" />
+        <img src={window.invoiceIcon} alt="invoice icon" />
         <p>Discreet invoicing</p>
       </div>
       
