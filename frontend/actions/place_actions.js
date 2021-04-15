50e503af61b0675c30f94c7261fd66d1fc3984a9
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-14 22:04:36 -0700

    Complete Utils and Actions for places

---
+++ frontend/actions/place_actions.js
@@ -0,0 +1,7 @@
+import {getAllPlaces, getPlace, deletePlace, updatePlace, createPlace} from '../utils/place_utils';
+
+export const RECEIVE_ALL_PLACES = "RECEIVE_ALL_PLACES";
+export const RECEIVE_PLACE = "RECEIVE_PLACE";
+export const DELETE_PLACE = "DELETE_PLACE";
+export const CREATE_PLACE = "CREATE_PLACE";
+
