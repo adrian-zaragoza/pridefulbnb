7aa56c26fb71989c61ebde7b94961476a85eb436
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-05-23 21:09:37 -0700

    Add searchbar to navigation bar

--- frontend/components/navigation_bar/navigation_bar.jsx
+++ frontend/components/navigation_bar/navigation_bar.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-const NavigationBar = ({ currentUser, logout, demoUser, props}) => {
+class NavigationBar extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      searchPlaceLocation: ""
+    }
+    this.handleSearchInput = this.handleSearchInput.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  }
+
+  handleSearchInput(e){
+    e.preventDefault();
+    this.setState({searchPlaceLocation: e.currentTarget.value})
+  }
+
+  handleSearchSubmit(e){
+    e.preventDefault();
+    sessionStorage.setItem('searchPlaceLocation', this.state.searchPlaceLocation)
+    this.props.history.push({pathname: '/search', state: {searchPlaceLocation: this.state.searchPlaceLocation}});
+  }
+
+
+
+  render(){
   let homeButton = undefined;
-  if(props.pathname != "/"){
+  let searchBar;
+  if(this.props.location.pathname != "/"){
     homeButton = (
       <Link className="nav-button" to="/">Home</Link>
+    )
+
+    searchBar = (
+          <form onSubmit={this.handleSearchSubmit}>
+            <label>
+              <input type="text" value={this.state.searchPlaceLocation} onChange={this.handleSearchInput} placeholder="Where do you want to go?"/>
+            </label>
+            {/* <button className="search-button" onClick={this.handleSearchSubmit}>SEARCH</button> */}
+            {/* {this.state.toggleSearch ? <SearchContainer searchPlaceLocation={this.state.searchPlaceLocation} /> : ""} */}
+          </form>
     )
   }
 
   let placesButton = undefined;
-  if(props.pathname != "/places"){
+  if(this.props.location.pathname != "/places"){
     placesButton = (<Link className="nav-button" to="/places">Places</Link>)
   }
   
@@ -20,29 +55,34 @@ const NavigationBar = ({ currentUser, logout, demoUser, props}) => {
       {placesButton}
       <Link className="nav-button" to="/signup">Sign Up</Link>
       <Link className="nav-button" to="/login">Log In</Link>
-      <button onClick={demoUser} className="nav-button">Demo Log In</button>
+      <button onClick={this.props.demoUser} className="nav-button">Demo Log In</button>
     </div>
   )
 
-  if(currentUser){
+  if(this.props.currentUser){
     display = (
       <div className ="nav-bar-links">
         {homeButton}
         {placesButton}
-        <p className ="user-name">{`Hi, ${currentUser.firstName}`}</p>
-        <button onClick={logout} className="nav-button">Logout</button>
+        <p className ="user-name">{`Hi, ${this.props.currentUser.firstName}`}</p>
+        <button onClick={this.props.logout} className="nav-button">Logout</button>
       </div>
     )
   }
 
   return (
     <div className="nav-bar">
-      <div>
+      <div className="logo-and-search">
         <Link className="logo" to="/"><h1>{"pridefulb&b"}</h1></Link>
+        {searchBar}
       </div>
       {display}
     </div>
-  );
+
+  )};
+
+
+
 };
 
-export default NavigationBar;
+export default withRouter(NavigationBar);
