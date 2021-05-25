b666ee219d970053b7295b731af84fefae59b5df
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-05-25 15:58:25 -0700

    Complete Booking Actions

---
+++ frontend/actions/booking_actions.js
@@ -0,0 +1,28 @@
+import * as APIUtil from '../utils/booking_utils';
+
+export const RECEIVE_USER_BOOKINGS = "RECEIVE_USER_BOOKINGS";
+export const RECEIVE_BOOKING = "RECEIVE_BOOKING";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+const receiveBooking = (booking) => ({
+  type: RECEIVE_BOOKING,
+  booking
+})
+
+const receiveUserBookings = (bookings) => ({
+  type: RECEIVE_USER_BOOKINGS,
+  bookings
+})
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+})
+
+export const fetchUserBookings = (userId) => (dispatch) =>(
+  APIUtil.getUserBookings(userId).then(bookings => (dispatch(receiveUserBookings(bookings))))
+);
+
+export const createBooking = (booking) => (dispatch) =>(
+  APIUtil.createBooking(booking).then(booking =>(dispatch(receiveBooking(booking))), errors => (dispatch(receiveErrors(errors.responseJSON))))
+);
