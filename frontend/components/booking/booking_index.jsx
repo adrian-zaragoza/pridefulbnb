e6b8cea95a148528418f0e7605617f05eaca0315
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-05-27 22:11:00 -0700

    Complete pages for upcoming travels and past travels

--- frontend/components/booking/booking_index.jsx
+++ frontend/components/booking/booking_index.jsx
@@ -5,7 +5,13 @@ import moment from 'moment';
 class BookingIndex extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      upcomingTravels: true,
+      pastTravels: false
+    }
+
     this.handleClickShow = this.handleClickShow.bind(this);
+    this.handleTravelsToggle = this.handleTravelsToggle.bind(this);
   }
 
   componentDidMount(){
@@ -18,47 +24,103 @@ class BookingIndex extends React.Component{
     this.props.history.push(`/places/${placeId}`);
   }
 
+  handleTravelsToggle(type, e){
+    e.preventDefault();
+    if(type === "upcoming" && this.state.upcomingTravels === false){
+      this.setState({
+        upcomingTravels: true,
+        pastTravels: false
+      })
+    }else if(type === "past" && this.state.pastTravels === false){
+      this.setState({
+        upcomingTravels: false,
+        pastTravels: true
+      })
+    }
+  }
+
   render(){
-    let travels;
+    let upcomingTravels;
+    let pastTravels;
+
     if(this.props.bookings && Object.values(this.props.bookings).length === 0){
-      travels = (<h1>No upcoming travel... yet</h1>)
+      upcomingTravels = (<h1>No upcoming travel... yet</h1>)
     }else if(this.props.bookings && Object.values(this.props.bookings).length > 0){
-      let bookingsArr = Object.values(this.props.bookings.bookings)
+      let bookingsArr = Object.values(this.props.bookings.bookings);
+      let upcomingTravelsArr = [];
+      let pastTravelsArr = [];
+
+      for(let i = 0; i < bookingsArr.length; i++){
+        if(moment(bookingsArr[i].endStay) > moment()){
+          upcomingTravelsArr.push(bookingsArr[i]);
+        }else{
+          pastTravelsArr.push(bookingsArr[i]);
+        }
+      }
       
-      travels = (
-      <div className="place-index">
-        {console.log("inside the travels variable")}
-        <div className="places-listings">
-          {
-            bookingsArr.map((booking) => {
-              let place = this.props.bookings.places[booking.placeId];
-            return(
-              <ul key={booking.id} className="place">
-                <img src={this.props.bookings.places[booking.placeId].imageUrl} onClick={(e)=>{ this.handleClickShow(booking.placeId, e)}} />
-                <li id="title" onClick={(e)=>{ this.handleClickShow(booking.placeId, e)}}>{place.title}</li>
-                <div className="place-details">
-                  <li>{`${place.typeOfPlace} •`}</li>
-                  <li>{`${place.location}`}</li>
-                </div>
-                <li>{`Trip ${moment(booking.startStay).format('L')} to ${moment(booking.endStay).format('L')}`}</li>
-                {/* <li>{new Date(booking.startStay) > new Date() ? <button onClick={()=>this.props.deleteBookingThunk(booking.id)}>Cancel Reservation</button> : ""}</li> */}
-                <li>{moment(booking.startStay) > moment() ? <button onClick={()=>this.props.deleteBookingThunk(booking.id)}>Cancel Reservation</button> : ""}</li>
-                {/* {console.log("start Date", new Date(`${booking.startStay} 00:00`))}
-                {console.log(booking.startStay)}
-                {console.log("Today", new Date())} */}
-                {/* {console.log("result", new Date(`${booking.startStay} 00:00`) > new Date())} */}
-              </ul>
-            )
-            } )
-          }
+      upcomingTravels = (
+        <div className="place-index">
+          <div className="upcoming-past-travels">
+            <button onClick={(e)=>this.handleTravelsToggle("upcoming", e)}>Upcoming travels</button>
+            <button onClick={(e)=>this.handleTravelsToggle("past", e)}>Past travels</button>
+          </div>
+          <div className="places-listings">
+            {
+              upcomingTravelsArr.map((booking) => {
+                let place = this.props.bookings.places[booking.placeId];
+              return(
+                <ul key={booking.id} className="place">
+                  <img src={this.props.bookings.places[booking.placeId].imageUrl} onClick={(e)=>{ this.handleClickShow(booking.placeId, e)}} />
+                  <li id="title" onClick={(e)=>{ this.handleClickShow(booking.placeId, e)}}>{place.title}</li>
+                  <div className="place-details">
+                    <li>{`${place.typeOfPlace} •`}</li>
+                    <li>{`${place.location}`}</li>
+                  </div>
+                  <li>{`Trip ${moment(booking.startStay).format('L')} to ${moment(booking.endStay).format('L')}`}</li>
+                  <li>{moment(booking.startStay) > moment() ? <button onClick={()=>this.props.deleteBookingThunk(booking.id)}>Cancel Reservation</button> : ""}</li>
+                </ul>
+              )
+              } )
+            }
+          </div>
         </div>
-      </div>
       )
+
+      
+      pastTravels = (
+        <div className="place-index">
+          <div className="upcoming-past-travels">
+            <button onClick={(e)=>this.handleTravelsToggle("upcoming", e)}>Upcoming travels</button>
+            <button onClick={(e)=>this.handleTravelsToggle("past", e)}>Past travels</button>
+          </div>
+          <div className="places-listings">
+            {
+              pastTravelsArr.map((booking) => {
+                let place = this.props.bookings.places[booking.placeId];
+              return(
+                <ul key={booking.id} className="place">
+                  <img src={this.props.bookings.places[booking.placeId].imageUrl} onClick={(e)=>{ this.handleClickShow(booking.placeId, e)}} />
+                  <li id="title" onClick={(e)=>{ this.handleClickShow(booking.placeId, e)}}>{place.title}</li>
+                  <div className="place-details">
+                    <li>{`${place.typeOfPlace} •`}</li>
+                    <li>{`${place.location}`}</li>
+                  </div>
+                  <li>{`Trip ${moment(booking.startStay).format('L')} to ${moment(booking.endStay).format('L')}`}</li>
+                  <li>{moment(booking.startStay) > moment() ? <button onClick={()=>this.props.deleteBookingThunk(booking.id)}>Cancel Reservation</button> : ""}</li>
+                </ul>
+              )
+              } )
+            }
+          </div>
+        </div>
+      )
+
+
     }
 
     return(
       <div>
-        {travels}
+        {this.state.upcomingTravels ? upcomingTravels : pastTravels}
       </div>
     )
   }
