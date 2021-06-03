9fd52923ed08b4eabde858909df9dfbac8277d02
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-06-03 16:14:43 -0700

    Add links to misterbnb phone app for google and apple buttons

--- frontend/components/home/phone_app.jsx
+++ frontend/components/home/phone_app.jsx
@@ -9,10 +9,10 @@ export default () => (
       <h1>Featured as App of the day by Apple (2018 & 2019)</h1>
       <div className="phone-app-buttons">
         <figure>
-          <img src={window.googleStore} alt="google store button"/>
+          <a href="https://play.google.com/store/apps/details?id=com.sfo84" target="_blank"><img src={window.googleStore} alt="google store button"/></a>
         </figure>
         <figure>
-          <img src={window.appleStore} alt="google store button" />
+          <a href="https://apps.apple.com/us/app/misterb-b-gay-travel-accommodations/id731435608" target="_blank"><img src={window.appleStore} alt="google store button" /></a>
         </figure>
       </div>
       <h2>Book with a tap. Download our app now.</h2>
