bc696a18c205d5c011bc81ee3a9b4d1ae7267366
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-07-21 15:01:05 -0700

    finish validation method valid review in the Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,10 +9,25 @@ class Review < ApplicationRecord
     foreign_key: :author_id,
     class_name: "User"
 
-  def review_count
+  def valid_review?
+    guest = self.author
+    place_bookings_count = guest.bookings.where("place_id = ?", self.place_id).count
+    
+    if place_bookings_count == 0
+      self.errors.add(:base, "You first need to stay at this place to leave a review")
+      return false
+    end
+    
+    guest_place_reviews_count = Review.where("author_id = ?", self.author_id).count
+    
+    #Checking if the number of reviewes by the guest for this place is greater than or equal to the number
+    #the guest has booked this place, the guest cannot leave another review.
+    if guest_place_reviews_count >= place_bookings_count
+      self.errors.add(:base, "You already left a review for this place")
+      return false
+    end
 
+    return true
   end
 
-  def place_in_bookings?
-  end
 end
