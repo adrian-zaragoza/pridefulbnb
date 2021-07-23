df51dc8d823d35605fc95190a2a889eed8de46bc
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-07-23 14:18:54 -0700

    Complete Review reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import placesReducer from './places_reducer';
 import bookingsReducer from './bookings_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   places: placesReducer,
-  bookings: bookingsReducer
+  bookings: bookingsReducer,
+  reviews: reviewsReducer
 })
 
 export default entitiesReducer;
---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_ALL_REVIEWS, RECEIVE_REVIEW, DELETE_REVIEW } from '../actions/review_actions';
+
+const reviewsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  const nextState = Object.assign({}, oldState);
+
+  switch(action.type){
+    case RECEIVE_ALL_REVIEWS:
+      return Object.assign({}, action.reviews, oldState);
+    case RECEIVE_REVIEW:
+      return 
+    case DELETE_REVIEW:
+      return delete nextState[action.reviewId]
+    default:
+      return oldState;
+  }
+};
+
+export default reviewsReducer;
